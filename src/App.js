7385f1a9c282398c94f961c59aee1d533e09699c
Himanshu Yadav <himanshu.yadav@aknamed.com>
2024-04-13 20:06:41 +0530

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
 
-function App() {
+
+export default function App() {
+  const [products, setProducts] = useState({ products: [] });
+  const targetElement = useRef({ pageNumber: 1 });
+
+  const observer = new IntersectionObserver((entries) => {
+    let entry = entries[0];
+    if (entry.isIntersecting) {
+      observer.unobserve(targetElement.current.observer);
+
+      let currentPageNumber = targetElement.current.pageNumber;
+      let totalPages = Math.ceil(products.total / 30);
+      if (currentPageNumber < totalPages) {
+        targetElement.current.pageNumber += 1;
+        fetchProducts(targetElement.current.pageNumber);
+      }
+    }
+  }, {
+    threshold: 0.5, // Call the callback when the element is at least 50% visible
+  });
+
+  /** used just for registering a new observer */
+  const registerIntersectionObserver = (element) => {
+    if (!element)
+      return;
+    targetElement.current.observer = element;
+    observer.observe(element);
+  }
+
+  const fetchProducts = async (pageNumber) => {
+    fetch(`https://dummyjson.com/products?limit=30&skip=${30 * (pageNumber - 1)}&select=title,price`)
+      .then(res => res.json())
+      .then(res => setProducts((prev) => ({ ...prev, products: [...prev.products, ...res.products], total: res.total })))
+  }
+
+  useEffect(() => {
+    fetchProducts(1)
+  }, []);
+
+
+  console.log("products ", products.total);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to React App
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ height: "200px", overflow: "scroll", marginTop: "100px", width: "500px", margin: "100px auto 0", border: "1px solid" }}
+      >
+        {products?.products?.map((prod, index) => (
+          <div {...(index === (targetElement.current.pageNumber * 15 - 1)) && observer && { ref: registerIntersectionObserver }}> {prod.title} </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
